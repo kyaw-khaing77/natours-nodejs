813e47bc691d2cfedb2b8714e18d96193c21b27a
kyawkhaing <kyawkhaing29581@gmail.com>
2022-08-21 16:10:21 +0630

    Implement CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -24,6 +25,12 @@ app.enable('trust proxy');
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 // 1) GLOBAL MIDDLEWARES
+//Implement CORS
+app.use(cors());
+
+app.options('*', cors());
+
+// 2) Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Set security HTTP headers
